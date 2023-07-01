170ebff125d7eebc1b8be8b519cdfcf580d91fa3
rlegler2012 <rlegler2012@hotmail.com>
2023-07-01 13:16:43 -0700

    setting up

---
+++ main_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views import View # <- View class to handle requests
+from django.http import HttpResponse # <- a class to handle sending a type of response
+
+
+# Create your views here.
+
+# Here we will be creating a class called Home and extending it from the View class
+class Home(View):
+
+    # Here we are adding a method that will be ran when we are dealing with a GET request
+    def get(self, request):
+        # Here we are returning a generic response
+        # This is similar to response.send() in express
+        return HttpResponse("Dogs Home")
