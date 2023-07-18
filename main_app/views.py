7441eb52711d639e01a38a1dc194225786789da9
rlegler2012 <rlegler2012@hotmail.com>
2023-07-18 15:10:13 -0700

    create and update routes working

--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,9 @@ from django.views import View
 from .models import Breed
 from django.http import HttpResponse # <- a class to handle sending a type of response
 from django.views.generic import TemplateView
-from django.views.generic.edit import CreateView
 from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse
 
 class Home(TemplateView):
     template_name = "home.html"
@@ -14,6 +15,8 @@ class About(TemplateView):
     template_name = "about.html"
 
 
+
+#### Breed List #########
 # class Breed:
 #     def __init__(self, name, img, description):
 #         self.name = name
@@ -21,12 +24,12 @@ class About(TemplateView):
 #         self.description = description
 
 
-# breeds = [
-#   Breed ("Little Spokane River", "https://assets.change.org/photos/9/lq/sr/HuLQsrEXlyAdApc-1600x900-noPad.jpg?1622402845",
-#           "Blah"),
-#   Breed ("Heyburn State Park",
-#           "https://lh3.googleusercontent.com/p/AF1QipOheBfE-Bixtqcbm3zw5N_wif3JiDaAzkiKEb1I=s1360-w1360-h1020", "lake"),
-# ]
+breeds = [
+  Breed ("Little Spokane River", "https://assets.change.org/photos/9/lq/sr/HuLQsrEXlyAdApc-1600x900-noPad.jpg?1622402845",
+          "Blah"),
+  Breed ("Heyburn State Park",
+          "https://lh3.googleusercontent.com/p/AF1QipOheBfE-Bixtqcbm3zw5N_wif3JiDaAzkiKEb1I=s1360-w1360-h1020", "lake"),
+]
 
 class BreedList(TemplateView):
     template_name = "breed_list.html"
@@ -44,15 +47,29 @@ class BreedList(TemplateView):
             context["header"] = "Trending Breeds"
         return context
 
+class BreedDetail(DetailView):
+    model = Breed
+    template_name = "breed_detail.html"
+    
 class BreedCreate(CreateView):
     model = Breed
     fields = ['name', 'img', 'description']
     template_name = "breed_create.html"
-    success_url = "/breeds/"
-
-class BreedDetail(DetailView):
+    # this will get the pk from the route and redirect to artist view
+    def get_success_url(self):
+        return reverse('breed_detail', kwargs={'pk': self.object.pk})
+        
+        
+class BreedUpdate(UpdateView):
     model = Breed
-    template_name = "breed_detail.html"
+    fields = ['name', 'img', 'description']
+    template_name = "breed_update.html"
+
+    def get_success_url(self):
+        return reverse('breed_detail', kwargs={'pk': self.object.pk})  
+    
+    
+    
 # class Activity:
 #     def __init__(self, name, image, description):
 #         self.name = name
